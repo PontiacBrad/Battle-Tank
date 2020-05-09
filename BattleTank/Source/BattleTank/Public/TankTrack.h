29d767ea37211d2f1e60fa695a2e04a8c7a61d6b
Bradley Gray <1bradleygray@gmail.com>
2020-05-08 20:00:23 -0400

    Add reverse throttle and physics materials for turning capability

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -20,5 +20,5 @@ public:
 	void SetThrottle(float Throttle);
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g acceleration
+	float TrackMaxDrivingForce = 40000000; // Assume 40 tonne tank, and 1g acceleration
 };
